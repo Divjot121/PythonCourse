83a247a020ec5abe5a3ccd36d04dfd5f370a2493
Divjot Singh <aroradivjotsingh@gmail.com>
2024-01-14 20:45:20 +0530

    Day 1

---
+++ Lecture 3.py
@@ -0,0 +1,16 @@
+# Variables And Data Types
+i = 5 # int datatype Var
+print(type(i))
+print(i)
+
+f = 12.3 # Float Datatype var
+print(type(f))
+print(f)
+
+b =  True # Bool datatype var
+print(type(b))
+print(b)
+
+s = "Hello, World!" # str datatype Var
+print(type(s))
+print(s)
